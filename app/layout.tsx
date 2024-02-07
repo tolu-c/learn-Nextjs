d9846ab6c0ed686625325b453c119a8ce3d52fa8
tolu-c <tolu.adeyemo7@gmail.com>
2024-02-07 11:44:04 +0100

    chap 16: improving meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Tolu's Acme Dashboard",
+    default: "Tolu's Acme Dashboard",
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-nu-five-40.vercel.app/'),
+};
 export default function RootLayout({
   children,
 }: {
